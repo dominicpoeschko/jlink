c6511f005e3d7c8457061d0c92df4de37e2a2269
dominic <mail@dominicp.de>
2023-02-13 16:37:50 +0100

    initial commit

---
+++ src/jlink/JLink.hpp
@@ -0,0 +1,209 @@
+#pragma once
+#include "jlink/JLinkDLL.h"
+
+#include <array>
+#include <cstddef>
+#include <exception>
+#include <fmt/format.h>
+#include <span>
+#include <string>
+#include <thread>
+
+struct JLink {
+public:
+    using Status = RTTStatus;
+
+private:
+    static void log(char const*) { /* fmt::print("log \"{}\"\n", msg);*/
+    }
+    static void log_error(char const* msg) { fmt::print("jlink error: \"{}\"\n", msg); }
+
+    void connect(std::string const& device, std::uint32_t speed) {
+        {
+            char const* ret = JLINK_OpenEx(log, log_error);
+            if(ret != nullptr) {
+                throw std::runtime_error{fmt::format("JLINK_OpenEx failed: {}", ret)};
+            }
+        }
+
+        {
+            int const ret = JLINK_TIF_Select(1);   //SWD
+            if(ret != 0) {
+                throw std::runtime_error{fmt::format("JLINK_TIF_Select failed: {}", ret)};
+            }
+        }
+
+        JLINK_SetSpeed(speed);
+        preConnectDisableDialogs();
+        execCommand(fmt::format("device = {}", device));
+        {
+            char const ret = JLINK_IsConnected();
+            if(ret == 0) {
+                int const ret2 = JLINK_Connect();
+                if(ret2 != 0) {
+                    throw std::runtime_error{fmt::format("JLINK_Connect failed: {}", ret2)};
+                }
+            }
+        }
+        //dummy to force connect
+        JLINK_IsHalted();
+
+        std::size_t trys = 10;
+
+        while(trys != 0) {
+            char const ret = JLINK_IsConnected();
+            if(ret == 0) {
+                --trys;
+            } else if(ret == 1) {
+                break;
+            } else {
+                throw std::runtime_error{fmt::format("JLINK_IsConnected failed: {:d}", ret)};
+            }
+            std::this_thread::sleep_for(std::chrono::milliseconds{100});
+        }
+        if(trys == 0) {
+            throw std::runtime_error{"JLINK_IsConnected failed: timeout"};
+        }
+        postConnectDisableDialogs();
+    }
+
+    void execCommand(std::string const& cmd) {
+        {
+            std::array<char, 1024> error_buffer;
+
+            int const ret
+              = JLINK_ExecCommand(cmd.c_str(), error_buffer.data(), error_buffer.size());
+            if(ret != 0) {
+                std::string_view error_msg{""};
+                if(ret > 0) {
+                    error_msg = std::string_view{error_buffer.data(), error_buffer.data() + ret};
+                };
+                throw std::runtime_error{
+                  fmt::format("JLINK_ExecCommand(\"{}\") failed: {}", cmd, error_msg)};
+            }
+        }
+    }
+
+    void checkError() {
+        int const ret = JLINK_HasError();
+        if(ret != 0) {
+            throw std::runtime_error{fmt::format("JLINK_HasError: {}", ret)};
+        }
+    }
+
+    void preConnectDisableDialogs() {
+        execCommand("DisableAutoUpdateFW");
+        execCommand("SilentUpdateFW");
+        execCommand("SuppressInfoUpdateFW");
+        execCommand("HideDeviceSelection 1");
+        execCommand("SuppressControlPanel");
+        execCommand("DisableInfoWinFlashDL");
+        execCommand("DisableInfoWinFlashBPs");
+    }
+    void postConnectDisableDialogs() { execCommand("SetBatchMode 1"); }
+
+public:
+    JLink(
+      std::string const& device,
+      std::uint32_t      speed,
+      std::string const& ip,
+      std::uint16_t      port = 19020) {
+        {
+            int const ret = JLINK_SelectIP(ip.c_str(), port);
+            if(ret != 0) {
+                throw std::runtime_error{
+                  fmt::format("JLINK_SelectIP({}, {}) failed: {}", ip, port, ret)};
+            }
+        }
+        connect(device, speed);
+        checkError();
+    }
+
+    explicit JLink(std::string const& device, std::uint32_t speed) {
+        {
+            int const ret = JLINK_SelectUSB(0);
+            if(ret != 0) {
+                throw std::runtime_error{fmt::format("JLINK_SelectUSB failed: {}", ret)};
+            }
+        }
+        connect(device, speed);
+        checkError();
+    }
+
+    ~JLink() { JLINK_Close(); }
+
+    void startRtt(std::uint32_t buffers, std::uint32_t configBlockAddress = 0) {
+        auto config = [](std::uint32_t address) {
+            RTTStart start{};
+            start.configBlockAddress = address;
+            int const ret            = JLINK_RTTERMINAL_Control(0, &start);   //start
+
+            if(ret < 0) {
+                throw std::runtime_error{fmt::format("JLINK_RTTERMINAL_Control failed: {}", ret)};
+            }
+        };
+        auto connect = [&]() {
+            std::size_t trys{100};
+            RTTStatus   status{readStatus()};
+            while(trys != 0
+                  && (status.isRunning == 0 || status.numUpBuffers != static_cast<int>(buffers)))
+            {
+                std::this_thread::sleep_for(std::chrono::milliseconds{10});
+                status = readStatus();
+                checkError();
+                --trys;
+            }
+            return trys != 0;
+        };
+        config(configBlockAddress);
+        if(!connect() && configBlockAddress != 0) {
+            config(0);
+            if(!connect()) {
+                throw std::runtime_error{"JLINK_RTTERMINAL_Control failed: timeout"};
+            }
+        }
+    }
+
+    std::span<std::byte> rttRead(std::uint32_t bufferNumber, std::span<std::byte> buffer) {
+        int const ret = JLINK_RTTERMINAL_Read(
+          bufferNumber,
+          reinterpret_cast<char*>(buffer.data()),
+          buffer.size());
+        if(ret < 0) {
+            throw std::runtime_error{fmt::format("JLINK_RTTERMINAL_Read failed: {}", ret)};
+        }
+        return std::span{buffer.data(), static_cast<std::size_t>(ret)};
+    }
+
+    void checkConnected() {
+        checkError();
+        char const ret = JLINK_IsConnected();
+        if(ret == 0) {
+            throw std::runtime_error{fmt::format("JLINK_Connected: {}", ret)};
+        }
+    }
+
+    void resetTarget() {
+        int const ret = JLINK_Reset();
+        if(ret < 0) {
+            throw std::runtime_error{fmt::format("JLINK_Reset: {}", ret)};
+        }
+    }
+
+    void flash(std::string const& hexFile) {
+        int const ret = JLINK_DownloadFile(hexFile.c_str(), 0);
+        if(ret < 0) {
+            throw std::runtime_error{fmt::format("JLINK_DownloadFile: {}", ret)};
+        }
+    }
+
+    RTTStatus readStatus() {
+        RTTStatus status{};
+
+        int const ret = JLINK_RTTERMINAL_Control(4, &status);   // get status
+        if(ret < 0) {
+            throw std::runtime_error{fmt::format("JLINK_RTTERMINAL_Control failed: {}", ret)};
+        }
+        return status;
+    }
+};
---
+++ src/jlink/JLinkDLL.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <array>
+#include <cstdint>
+
+extern "C" {
+
+struct RTTStart {
+    std::uint32_t                configBlockAddress;
+    std::array<std::uint32_t, 3> padding;
+};
+
+struct RTTStatus {
+    std::uint32_t numBytesTransferred;
+    std::uint32_t numBytesRead;
+    int           hostOverflowCount;
+    int           isRunning;
+    int           numUpBuffers;
+    int           numDownBuffers;
+    std::uint32_t overflowMask;
+    std::uint32_t padding;
+};
+
+using log_callback = void(char const*);
+
+char const* JLINK_OpenEx(log_callback* log, log_callback* errorLog);
+int         JLINK_TIF_Select(int interface);   //JTAG 0 SWD 1
+void        JLINK_SetSpeed(std::uint32_t Speed);
+char        JLINK_IsConnected();
+int         JLINK_Connect();
+char        JLINK_IsHalted();
+int         JLINK_ExecCommand(char const* in, char* out, int bufferSize);
+int         JLINK_HasError();
+void        JLINK_Close();
+char        JLINK_SelectUSB(int port);
+char        JLINK_SelectIP(char const* host, int port);
+int         JLINK_Reset();
+int         JLINK_DownloadFile(const char* sFileName, std::uint32_t Addr);
+int         JLINK_RTTERMINAL_Control(std::uint32_t command, void*);   //start 0 getStatus 4
+int JLINK_RTTERMINAL_Read(std::uint32_t bufferIndex, char* buffer, std::uint32_t bufferSize);
+}
