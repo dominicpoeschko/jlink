18b9adc27cbfb05b808bed80a58fe80ed51af1af
dominic <mail@dominicp.de>
2023-02-16 12:15:35 +0100

    removed fmt dependency

--- src/jlink/JLink.hpp
+++ src/jlink/JLink.hpp
@@ -3,8 +3,8 @@
 
 #include <array>
 #include <cstddef>
+#include <cstdio>
 #include <exception>
-#include <fmt/format.h>
 #include <span>
 #include <string>
 #include <thread>
@@ -14,34 +14,39 @@ public:
     using Status = RTTStatus;
 
 private:
-    static void log(char const*) { /* fmt::print("log \"{}\"\n", msg);*/
+    static void log(char const* msg) {
+        auto const s = std::string{"jlink_log: "} + msg;
+        std::fputs(s.c_str(), stderr);
     }
-    static void log_error(char const* msg) { fmt::print("jlink error: \"{}\"\n", msg); }
+    static void log_error(char const* msg) {
+        auto const s = std::string{"jlink_log_error: "} + msg;
+        std::fputs(s.c_str(), stderr);
+    }
 
     void connect(std::string const& device, std::uint32_t speed) {
         {
             char const* ret = JLINK_OpenEx(log, log_error);
             if(ret != nullptr) {
-                throw std::runtime_error{fmt::format("JLINK_OpenEx failed: {}", ret)};
+                throw std::runtime_error{std::string{"JLINK_OpenEx failed: "} + ret};
             }
         }
 
         {
             int const ret = JLINK_TIF_Select(1);   //SWD
             if(ret != 0) {
-                throw std::runtime_error{fmt::format("JLINK_TIF_Select failed: {}", ret)};
+                throw std::runtime_error{"JLINK_TIF_Select failed: " + std::to_string(ret)};
             }
         }
 
         JLINK_SetSpeed(speed);
         preConnectDisableDialogs();
-        execCommand(fmt::format("device = {}", device));
+        execCommand("device = " + device);
         {
             char const ret = JLINK_IsConnected();
             if(ret == 0) {
                 int const ret2 = JLINK_Connect();
                 if(ret2 != 0) {
-                    throw std::runtime_error{fmt::format("JLINK_Connect failed: {}", ret2)};
+                    throw std::runtime_error{"JLINK_Connect failed: " + std::to_string(ret2)};
                 }
             }
         }
@@ -57,7 +62,8 @@ private:
             } else if(ret == 1) {
                 break;
             } else {
-                throw std::runtime_error{fmt::format("JLINK_IsConnected failed: {:d}", ret)};
+                throw std::runtime_error{
+                  "JLINK_IsConnected failed: " + std::to_string(static_cast<int>(ret))};
             }
             std::this_thread::sleep_for(std::chrono::milliseconds{100});
         }
@@ -74,12 +80,11 @@ private:
             int const ret
               = JLINK_ExecCommand(cmd.c_str(), error_buffer.data(), error_buffer.size());
             if(ret != 0) {
-                std::string_view error_msg{""};
+                std::string error_msg{""};
                 if(ret > 0) {
                     error_msg = std::string_view{error_buffer.data(), error_buffer.data() + ret};
                 };
-                throw std::runtime_error{
-                  fmt::format("JLINK_ExecCommand(\"{}\") failed: {}", cmd, error_msg)};
+                throw std::runtime_error{"JLINK_ExecCommand(\"" + cmd + "\") failed: " + error_msg};
             }
         }
     }
@@ -87,7 +92,7 @@ private:
     void checkError() {
         int const ret = JLINK_HasError();
         if(ret != 0) {
-            throw std::runtime_error{fmt::format("JLINK_HasError: {}", ret)};
+            throw std::runtime_error{"JLINK_HasError: " + std::to_string(ret)};
         }
     }
 
@@ -112,7 +117,8 @@ public:
             int const ret = JLINK_SelectIP(ip.c_str(), port);
             if(ret != 0) {
                 throw std::runtime_error{
-                  fmt::format("JLINK_SelectIP({}, {}) failed: {}", ip, port, ret)};
+                  "JLINK_SelectIP(" + ip + ", " + std::to_string(static_cast<int>(port))
+                  + ") failed: " + std::to_string(ret)};
             }
         }
         connect(device, speed);
@@ -123,7 +129,7 @@ public:
         {
             int const ret = JLINK_SelectUSB(0);
             if(ret != 0) {
-                throw std::runtime_error{fmt::format("JLINK_SelectUSB failed: {}", ret)};
+                throw std::runtime_error{"JLINK_SelectUSB failed: " + std::to_string(ret)};
             }
         }
         connect(device, speed);
@@ -139,7 +145,7 @@ public:
             int const ret            = JLINK_RTTERMINAL_Control(0, &start);   //start
 
             if(ret < 0) {
-                throw std::runtime_error{fmt::format("JLINK_RTTERMINAL_Control failed: {}", ret)};
+                throw std::runtime_error{"JLINK_RTTERMINAL_Control failed: " + std::to_string(ret)};
             }
         };
         auto connect = [&]() {
@@ -170,7 +176,7 @@ public:
           reinterpret_cast<char*>(buffer.data()),
           buffer.size());
         if(ret < 0) {
-            throw std::runtime_error{fmt::format("JLINK_RTTERMINAL_Read failed: {}", ret)};
+            throw std::runtime_error{"JLINK_RTTERMINAL_Read failed: " + std::to_string(ret)};
         }
         return std::span{buffer.data(), static_cast<std::size_t>(ret)};
     }
@@ -179,21 +185,21 @@ public:
         checkError();
         char const ret = JLINK_IsConnected();
         if(ret == 0) {
-            throw std::runtime_error{fmt::format("JLINK_Connected: {}", ret)};
+            throw std::runtime_error{"JLINK_Connected: " + std::to_string(static_cast<int>(ret))};
         }
     }
 
     void resetTarget() {
         int const ret = JLINK_Reset();
         if(ret < 0) {
-            throw std::runtime_error{fmt::format("JLINK_Reset: {}", ret)};
+            throw std::runtime_error{"JLINK_Reset: " + std::to_string(ret)};
         }
     }
 
     void flash(std::string const& hexFile) {
         int const ret = JLINK_DownloadFile(hexFile.c_str(), 0);
         if(ret < 0) {
-            throw std::runtime_error{fmt::format("JLINK_DownloadFile: {}", ret)};
+            throw std::runtime_error{"JLINK_DownloadFile: " + std::to_string(ret)};
         }
     }
 
@@ -202,7 +208,7 @@ public:
 
         int const ret = JLINK_RTTERMINAL_Control(4, &status);   // get status
         if(ret < 0) {
-            throw std::runtime_error{fmt::format("JLINK_RTTERMINAL_Control failed: {}", ret)};
+            throw std::runtime_error{"JLINK_RTTERMINAL_Control failed: " + std::to_string(ret)};
         }
         return status;
     }
